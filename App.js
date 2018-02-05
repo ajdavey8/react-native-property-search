4c0d89d356aa2d7fdc21622c3597c3ac9b46d031
Andrew Davey <ajdavey8@gmail.com>
2018-02-05 20:43:26 +0000

    First Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+    return React.createElement(Text, {style: styles.description}, "Search for houses to buy");
+  }  
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
