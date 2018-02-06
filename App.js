4fbe5a2451abc6a105139179a3d4762443904801
Andrew Davey <ajdavey8@gmail.com>
2018-02-06 14:13:58 +0000

    Extract searchpage into own file and add styling

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import SearchPage from './SearchPage.js';
 import React, { Component } from 'react';
 import {
   Platform,
@@ -8,25 +9,6 @@ import {
   View
 } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
-
-class SearchPage extends Component<{}> {
-  static navigationOptions = {
-    title: 'Property Finder',
-  };
-
-  render() {
-    return <Text style={styles.description}>Search for houses to buy!</Text>;
-  }
-}
-
-const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
-  },
-});
 
 const App = StackNavigator({
   Home: { screen: SearchPage },
